9c266f490c4b034d0cd52684623baf7189b5db7d
Jacek <jacunio122@gmail.com>
2024-01-30 21:11:28 +0100

    Init + first changes

---
+++ app.js
@@ -0,0 +1,23 @@
+const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter")
+const { OpenAI } = require("@langchain/openai");
+const fs = require('node:fs');
+
+const main = async function() {
+    try {
+        fs.readFile('TFT-info.txt', 'utf8', async (err, data) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            const text = data;
+            const splitter = new RecursiveCharacterTextSplitter();
+            const output = await splitter.createDocuments([text]);
+            console.log(output);
+          });
+    
+    }catch (err) {
+        console.log(err);
+    }
+  }
+  
+  main();
